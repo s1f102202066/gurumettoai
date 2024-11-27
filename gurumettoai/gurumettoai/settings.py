250bceaefb5dd208aea0125ca96b76aecba3118c
Tsubasa <s1f102202066@iniad.org>
2024-11-27 15:17:06 +0900

    commit

--- gurumettoai/gurumettoai/settings.py
+++ gurumettoai/gurumettoai/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-80-ch$l9sap3&x**zx=9hsiwb=1t1n78!l#%yr$o7jjt3(jvpf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
