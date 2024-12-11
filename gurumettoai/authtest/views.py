a34fba8f5a95d1b4e2fd72235c8bf4a11b7e31f2
s1f102200569 <s1f102200569@iniad.org>
2024-12-11 15:41:46 +0900

    新規登録画面の追加

--- gurumettoai/authtest/views.py
+++ gurumettoai/authtest/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'authtest/home.html', {})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'アカウントが作成されました！ログインしてください。')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authtest/register.html', {'form': form})
